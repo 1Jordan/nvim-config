514c61fb6683189811510d9690933f10a10cacce
Jordan Schwartz <1jordanschwartz@gmail.com>
2023-01-13 12:16:13 -0500

    updated which key lsp keymaps

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -57,22 +57,30 @@ M.setup = function()
 end
 
 local function lsp_keymaps(bufnr)
-	local opts = { noremap = true, silent = true }
-	local keymap = vim.api.nvim_buf_set_keymap
-	keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
-	keymap(bufnr, "n", "<leader>lI", "<cmd>Mason<cr>", opts)
-	keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-	keymap(bufnr, "n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
-	keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
-	keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-	keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  which_key.register({
+    D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Declaration" },
+    d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
+    K = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover info" },
+    I = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Implementation" },
+    r = { "<cmd>lua vim.lsp.buf.references()<CR>", "References" },
+    l = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Diagnostic" }
+  }, { prefix = "g" })
+
+  which_key.register({
+    l = {
+      name = "LSP",
+      i = { "<cmd>LspInfo<cr>", "Lsp Info" },
+      I = { "<cmd>Mason<cr>", "Mason" },
+      a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code action" },
+      j = { "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", "Next diagnostic" },
+      k = { "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", "Prev diagnostic" },
+      r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+      s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
+      q = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "Set loc list" }
+    }
+  }, { prefix = "<leader>" })
+
+
 end
 
 M.on_attach = function(client, bufnr)
