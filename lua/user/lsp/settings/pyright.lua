2e3d7427a45b774308b0a98aad9d65432f25850d
Jordan Schwartz <1jordanschwartz@gmail.com>
2023-01-14 16:36:06 -0500

    made pyright lsp only use container if in duke ev project

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,33 +1,56 @@
 local status_ok, lspcontainers = pcall(require, "lspcontainers")
 if not status_ok then
-	return
+  return
 end
 
-return {
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "off",
-      },
+local ros_project = "/home/jordan/autonomous-vehicle"
+
+local settings = {
+  python = {
+    analysis = {
+      typeCheckingMode = "off",
     },
   },
-	before_init = function(params)
-		params.processId = vim.NIL
-	end,
-	root_dir = require("lspconfig/util").root_pattern(".git", vim.fn.getcwd()),
-	cmd = lspcontainers.command("pyright", {
-		image = "dev_image_lsp:foxy",
-		cmd = function(runtime, volume, image)
-			return {
-				runtime,
-				"container",
-				"run",
-				"--interactive",
-				"--rm",
-				"--volume",
-				volume,
-				image,
-			}
-		end,
-	}),
 }
+
+local ros_development = {
+  settings = settings,
+  before_init = function(params)
+    params.processId = vim.NIL
+  end,
+  root_dir = require("lspconfig/util").root_pattern(".git", vim.fn.getcwd()),
+  cmd = lspcontainers.command("pyright", {
+    image = "dev_image_lsp:foxy",
+    cmd = function(runtime, volume, image)
+      return {
+        runtime,
+        "container",
+        "run",
+        "--interactive",
+        "--rm",
+        "--volume",
+        volume,
+        image,
+      }
+    end,
+  }),
+}
+
+local normal = {
+  settings = settings
+}
+
+local function startswith(text, prefix)
+    return text:find(prefix, 1, true) == 1
+end
+
+local path = vim.api.nvim_buf_get_name(0)
+
+
+if startswith(path, ros_project) then
+  return ros_development
+else
+  return normal
+end
+
+
